7747b2af9caa690256bf4d8f2235ec6e7e4e5b5a
Xetroc <mattholtti@gmail.com>
2015-05-13 13:06:22 +0200

    Revert "mere fiks prut"
    This reverts commit ddc8e979cd87a550757646393b8e596de0415611.

--- Delfin/src/FileHandler.java
+++ Delfin/src/FileHandler.java
@@ -13,7 +13,7 @@ public class FileHandler {
 	
 	public FileHandler(String fileName, SerializableList serializableList){
 		
-		this.fileName = fileName+".bin";
+		this.fileName = fileName+".ser";
 		
 		this.file = new File(this.fileName);
 		
@@ -40,7 +40,6 @@ public class FileHandler {
 		
 		try 
 		{
-			System.out.println("Vi skriver her");
 			FileOutputStream fous = new FileOutputStream(file);
 			ObjectOutputStream oos = new ObjectOutputStream(fous);
 		    oos.writeObject(serializableList);
@@ -53,17 +52,12 @@ public class FileHandler {
 		}
 	}
 	
-	public File getFile(){
-		return this.file;
-	}
-	
 	public SerializableList read(){
 		
 		SerializableList serList = null;
 		
 		try 
 		{
-			System.out.println("Vi starter her");
 			FileInputStream streamIn = new FileInputStream(file);
 		    ObjectInputStream objectinputstream = new ObjectInputStream(streamIn);    
 		    serList = (SerializableList) objectinputstream.readObject();
