f0d70b8d17b27b34d30290cd24b0d393b9e4d823
Xetroc <mattholtti@gmail.com>
2015-05-13 10:30:15 +0200

    Revert "Changed member added compmember disciplin excmember pasmember swimlength swimresult"
    This reverts commit a2484897567e5d78857d323c7df34bfd0d90c522.

--- Delfin/src/ExcMember.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-
-public class ExcMember extends Member{
-	public ExcMember(String fName, String lName, int age, boolean pMember, LocalDate cDate){
-		super(fName, lName, age, pMember, cDate);
-	}
-}
